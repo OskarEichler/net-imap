99606da9fb9602ae68d19a1d3e824f1af4e1e1d8
nick evans <nick@rubinick.dev>
2026-08-29 15:44:08 -0400

    ✅ Add `SearchResult#eql?` and `#hash` tests
    This was motivated by a bug report (#738), and related to an earlier bug
    report (#514).  The bug itself is identified by the pending tests.

--- test/net/imap/test_search_result.rb
+++ test/net/imap/test_search_result.rb
@@ -19,6 +19,14 @@ class SearchDataTests < Net::IMAP::TestCase
     assert_equal unsorted, array
   end
 
+  test "#eql? doesn't ignore the order of elements" do
+    unsorted = SearchResult[4, 2, 2048, 99]
+    sorted   = SearchResult[2, 4, 99, 2048]
+    refute_operator sorted,   :eql?, unsorted
+    refute_operator unsorted, :eql?, sorted
+    refute_equal sorted.hash, unsorted.hash
+  end
+
   test "#== checks modseq" do
     unsorted = SearchResult[4, 2, 2048, 99, modseq: 99_999]
     sorted   = SearchResult[2, 4, 99, 2048, modseq: 99_999]
@@ -30,6 +38,46 @@ class SearchDataTests < Net::IMAP::TestCase
     refute_equal nomodseq, sorted
   end
 
+  test "#eql? and #hash always compare modseq" do
+    array = [2, 4, 99, 2048]
+    no_modseq    = SearchResult[*array]
+    with_modseq  = SearchResult[*array, modseq: 4000]
+    other_modseq = SearchResult[*array, modseq: 5000]
+    assert_operator    no_modseq, :eql?,    no_modseq.dup
+    assert_operator  with_modseq, :eql?,  with_modseq.dup
+    assert_operator other_modseq, :eql?, other_modseq.dup
+    assert_equal    no_modseq.hash,    no_modseq.dup.hash
+    assert_equal  with_modseq.hash,  with_modseq.dup.hash
+    assert_equal other_modseq.hash, other_modseq.dup.hash
+
+    pend "Bug #738" do
+      refute_operator    no_modseq, :eql?,  with_modseq
+    end
+    refute_operator  with_modseq, :eql?,    no_modseq
+    refute_operator  with_modseq, :eql?, other_modseq
+    refute_operator other_modseq, :eql?,    no_modseq
+    refute_operator other_modseq, :eql?,  with_modseq
+    refute_equal    no_modseq.hash,  with_modseq.hash
+    refute_equal  with_modseq.hash, other_modseq.hash
+    refute_equal other_modseq.hash,    no_modseq.hash
+  end
+
+  test "SearchResult.new(nz_numbers) == / eql? SearchResult[*nz_numbers]" do
+    array = [1, 5, 20, 3, 98]
+    new_with_array_arg   = SearchResult.new(array)
+    splatted_in_brackets = SearchResult[*array]
+    [nil, 12345].each do |modseq|
+      splatted_in_brackets = SearchResult[*array, modseq:]
+      new_with_array_arg   = SearchResult.new(array, modseq:)
+      assert_equal    splatted_in_brackets,        new_with_array_arg
+      assert_equal    new_with_array_arg,        splatted_in_brackets
+      assert_operator splatted_in_brackets, :eql?, new_with_array_arg
+      assert_operator new_with_array_arg, :eql?, splatted_in_brackets
+      assert_equal    splatted_in_brackets.hash,   new_with_array_arg.hash
+      assert_equal    new_with_array_arg.hash,   splatted_in_brackets.hash
+    end
+  end
+
   test "SearchResult[*nz_numbers] == Array[*nz_numbers]" do
     array  = [1, 5, 20, 3, 98]
     result = SearchResult[*array]
@@ -37,24 +85,50 @@ class SearchDataTests < Net::IMAP::TestCase
     assert_equal result, array
   end
 
-  test "SearchResult.new(nz_numbers) == Array.new(nz_numbers)" do
-    nz_numbers = [11, 35, 39, 1083, 958]
-    result = SearchResult.new(nz_numbers)
-    array  = Array.new(nz_numbers)
-    assert_equal array, result
-    assert_equal result, array
+  test "SearchResult[*nz_numbers] not eql? Array[*nz_numbers]" do
+    array  = [1, 5, 20, 3, 98]
+    result = SearchResult[*array]
+    pend "See comments on #738" do
+      refute_operator result, :eql?, array
+    end
+    pend "See comments on #738" do
+      refute_operator result.hash, :eql?, array.hash
+    end
   end
 
-  test "SearchResult[*nz_numbers, modseq: nz_number] != Array[*nz_numbers]" do
+  # NOTE: this subclass is NOT overriding #==, #hash, or #eql?
+  Subclass = Class.new(SearchResult)
+
+  test "SearchResult[...] == / eql? Subclass[...]" do
+    array    = [1, 5, 20, 3, 98]
+    result   = SearchResult[*array]
+    subclass = Subclass[*array]
+    assert_operator result, :eql?, subclass
+    assert_equal result.hash, subclass.hash
+    modseq   = 12345
+    result   = SearchResult[*array, modseq:]
+    subclass = Subclass[*array, modseq:]
+    pend "See comments on #738" do
+      assert_operator result, :eql?, subclass
+    end
+    pend "See comments on #738" do
+      assert_equal result.hash, subclass.hash
+    end
+  end
+
+  test "SearchResult[*nz_numbers, modseq: nz_number] != / not eql? Array[*nz_numbers]" do
     array  = [1, 5, 20, 3, 98]
     result = SearchResult[*array, modseq: 123456]
     refute_equal result, array
+    refute_operator result, :eql?, array
+    refute_equal result.hash, array.hash
   end
 
-  test "Array[*nz_numbers] == SearchResult[*nz_numbers, modseq: nz_number]" do
+  test "Array[*nz_numbers] == / eql? SearchResult[*nz_numbers, modseq: nz_number]" do
     array  = [1, 5, 20, 3, 98]
     result = SearchResult[*array, modseq: 123456]
     assert_equal array, result
+    assert_operator array, :eql?, result
   end
 
   test "SearchResult[*nz_numbers] == Array[*differently_sorted]" do
@@ -63,10 +137,19 @@ class SearchDataTests < Net::IMAP::TestCase
     assert_equal result, array
   end
 
-  test "Array[*nz_numbers] != SearchResult[*differently_sorted]" do
+  test "SearchResult[*nz_numbers] not eql? Array[*differently_sorted]" do
+    array  = [1, 5, 20, 3, 98]
+    result = SearchResult[*array.reverse]
+    refute_operator result, :eql?, array
+    refute_equal result.hash, array.hash
+  end
+
+  test "Array[*nz_numbers] != / not eql? SearchResult[*differently_sorted]" do
     array  = [1, 5, 20, 3, 98]
     result = SearchResult[*array.reverse]
     refute_equal array, result
+    refute_operator result, :eql?, array
+    refute_equal array.hash, result.hash
   end
 
   test "#inspect" do
